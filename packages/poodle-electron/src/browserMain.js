447679729a07ede5a498da00711825e834d0fc1b
Jesse Hallett <jesse@sitr.us>
2017-01-17 21:50:23 -0400

    Add React - I forgot that JSX compiles to `React.createElement` calls

--- packages/poodle-electron/src/browserMain.js
+++ packages/poodle-electron/src/browserMain.js
@@ -3,6 +3,7 @@
 import ApolloClient       from 'apollo-client'
 import { reduxStore }     from 'poodle-core'
 import App                from 'poodle-core/lib/components/App'
+import * as React         from 'react'
 import { ApolloProvider } from 'react-apollo'
 import * as ReactDOM      from 'react-dom'
 
