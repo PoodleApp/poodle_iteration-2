efb4636c4f7624a56ad587013ee014d2303db2eb
Jesse Hallett <jesse@sitr.us>
2017-01-17 22:36:45 -0400

    Import root component from 'poodle-core'; install tap event plugin

--- packages/poodle-electron/src/browserMain.js
+++ packages/poodle-electron/src/browserMain.js
@@ -1,20 +1,15 @@
 /* @flow */
 
-import ApolloClient       from 'apollo-client'
-import { reduxStore }     from 'poodle-core'
-import App                from 'poodle-core/lib/components/App'
-import * as React         from 'react'
-import { ApolloProvider } from 'react-apollo'
-import * as ReactDOM      from 'react-dom'
+import * as React           from 'react'
+import * as ReactDOM        from 'react-dom'
+import injectTapEventPlugin from 'react-tap-event-plugin'
+import { RootComponent }    from 'poodle-core'
 
-const client = new ApolloClient()
-const store  = reduxStore(client)
+// Adds support for `onTouchTap` to React components
+// TODO: this should not be required both here and in `poodle-core`;
+// but there may be an issue with multiple copies of React when using `yarn link`
+injectTapEventPlugin()
 
 export function main(root: Element) {
-  ReactDOM.render(
-    <ApolloProvider store={store} client={client}>
-      <App />
-    </ApolloProvider>,
-    root
-  )
+  ReactDOM.render(<RootComponent />, root)
 }
