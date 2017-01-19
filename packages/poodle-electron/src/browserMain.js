261d04478795fa0cd7dae8a31ee7e557b7b7229f
Jesse Hallett <jesse@sitr.us>
2017-01-19 10:20:26 -0400

    Remove need for 'react-tap-event-plugin' by fixing multiple-React issue

--- packages/poodle-electron/src/browserMain.js
+++ packages/poodle-electron/src/browserMain.js
@@ -2,13 +2,7 @@
 
 import * as React           from 'react'
 import * as ReactDOM        from 'react-dom'
-import injectTapEventPlugin from 'react-tap-event-plugin'
 import { RootComponent }    from 'poodle-core'
-
-// Adds support for `onTouchTap` to React components
-// TODO: this should not be required both here and in `poodle-core`;
-// but there may be an issue with multiple copies of React when using `yarn link`
-injectTapEventPlugin()
 
 export function main(root: Element) {
   ReactDOM.render(<RootComponent />, root)
