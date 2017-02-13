895c421e50deaf52e8d69add48ab5240ceb7a84d
Jesse Hallett <jesse@sitr.us>
2017-02-12 23:37:30 -0400

    Add `lastActiveTime` and `participants` properties to `Conversation` type

--- packages/poodle-service/src/schema/Conversation.js
+++ packages/poodle-service/src/schema/Conversation.js
@@ -2,6 +2,7 @@
 
 import ArfeConversation   from 'arfe/lib/models/Conversation'
 import * as graphql       from 'graphql'
+import GraphQLDateTime    from 'graphql-custom-datetype'
 import Connection         from 'imap'
 import * as m             from 'mori'
 import { searchByThread } from '../actions/google'
@@ -16,6 +17,31 @@ export type ConversationData = {
   conversation: ArfeConversation,
 }
 
+const URI = new graphql.GraphQLNonNull(graphql.GraphQLString)
+
+const Address = new graphql.GraphQLObjectType({
+  name: 'Address',
+  description: 'Email address and name',
+  fields: {
+    displayName: {
+      type: graphql.GraphQLString,
+      description: 'Name, or email address if no name is available',
+    },
+    email: {
+      type: graphql.GraphQLString,
+      description: 'Email address',
+    },
+    host: {
+      type: graphql.GraphQLString,
+      description: 'Host portion of email address',
+    },
+    uri: {
+      type: URI,
+      description: '`mailto:` URI constructed from email address',
+    },
+  },
+})
+
 export default new graphql.GraphQLObjectType({
   name: 'Conversation',
   description: 'Activities derived from a message thread according to the ARFE protocol',
@@ -24,6 +50,20 @@ export default new graphql.GraphQLObjectType({
       type: new graphql.GraphQLNonNull(graphql.GraphQLString),
       description: 'URI of the first message in the conversation',
       resolve({ conversation }: ConversationData) { return conversation.id },
+    },
+    lastActiveTime: {
+      type: GraphQLDateTime,
+      description: 'Time and date of latest activity in the conversation',
+      resolve({ conversation }: ConversationData) {
+        return conversation.lastActiveTime
+      },
+    },
+    participarts: {
+      type: new graphql.GraphQLList(Address),
+      description: 'People or entities who have sent or received activities in the conversation',
+      resolve({ conversation }: ConversationData) {
+        return m.intoArray(conversation.flatParticipants)
+      }
     },
     subject: {
       type: LanguageValue,
