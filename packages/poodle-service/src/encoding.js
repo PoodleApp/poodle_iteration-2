cd107059ecdeb43806c502f8c8a04813154ef4e3
Jesse Hallett <jesse@sitr.us>
2017-03-02 14:02:26 -0400

    Handle '7BIT' and 'BASE64' encodings

--- packages/poodle-service/src/encoding.js
+++ packages/poodle-service/src/encoding.js
@@ -1,12 +1,15 @@
 /* @flow */
 
+import base64     from 'base64-stream'
 import * as libqp from 'libqp'
 
 import type { ReadStream } from 'fs'
 
 export function decode(encoding: string, content: ReadStream): ReadStream {
   switch (encoding) {
+    case '7BIT':             return content
+    case 'BASE64':           return content.pipe(base64.decode())
     case 'QUOTED-PRINTABLE': return content.pipe(new libqp.Decoder)
-    default: throw new Error(`unknown encoding: ${encoding}`)
+    default:                 throw new Error(`unknown encoding: ${encoding}`)
   }
 }
