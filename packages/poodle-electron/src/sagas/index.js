3d87d36cd4f6e7ed2ac131f279b69e560f3d2a52
Jesse Hallett <jesse@sitr.us>
2017-07-26 11:35:13 -0400

    poodle-electron: root saga must be a generator

--- packages/poodle-electron/src/sagas/index.js
+++ packages/poodle-electron/src/sagas/index.js
@@ -3,7 +3,10 @@
 import keytar from 'keytar'
 import * as auth from 'poodle-core/lib/actions/auth'
 import authSaga from 'poodle-core/lib/sagas/auth'
+import { fork } from 'redux-saga/effects'
 import * as oauth from '../oauth'
+
+import type { Effect } from 'redux-saga'
 
 // Generator type parameters are of the form: `Generator<+Yield,+Return,-Next>`
 
@@ -36,10 +39,14 @@ async function saveAccount (account: auth.Account) {
   localStorage.setItem('account', JSON.stringify(account))
 }
 
-export default authSaga({
+const authDeps = {
   getAccessToken: oauth.getAccessToken,
   loadAccessToken,
   storeAccessToken,
   loadAccount,
   saveAccount
-})
+}
+
+export default function * root (): Generator<Effect, void, any> {
+  yield [fork(authSaga, authDeps)]
+}
