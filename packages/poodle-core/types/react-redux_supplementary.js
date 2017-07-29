ca771419b22b05a95dcf99025fd5f1b9e22c1e0a
Jesse Hallett <jesse@sitr.us>
2017-07-29 18:20:36 -0400

    Install react & redux deps directly in poodle-electron

---
+++ packages/poodle-core/types/react-redux_supplementary.js
@@ -0,0 +1,7 @@
+declare module 'react-redux/lib/connect/mapStateToProps' {
+  declare module.exports: any
+}
+
+declare module 'react-redux/lib/utils/shallowEqual' {
+  declare module.exports: any
+}
