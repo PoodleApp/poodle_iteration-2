b905c0a271581b5e86f6171014e699724eb7b311
Jesse Hallett <jesse@sitr.us>
2017-08-10 15:02:43 -0400

    poodle-service: handle 8BIT message encoding

--- packages/poodle-service/src/encoding.js
+++ packages/poodle-service/src/encoding.js
@@ -8,6 +8,7 @@ import type { Readable } from 'stream'
 export function decode(encoding: string, content: Readable): Readable {
   switch (encoding) {
     case '7BIT':             return content
+    case '8BIT':             return content
     case 'BASE64':           return content.pipe(base64.decode())
     case 'QUOTED-PRINTABLE': return content.pipe(new libqp.Decoder)
     default:                 throw new Error(`unknown encoding: ${encoding}`)
