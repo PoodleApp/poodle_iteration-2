8328b91ebae3843c6b5c7103eeadf61e9f726f69
Jesse Hallett <jesse@sitr.us>
2017-09-14 13:53:20 -0400

    poodle-electron: pass database reference to IMAP Client

--- packages/poodle-electron/src/imapClient.js
+++ packages/poodle-electron/src/imapClient.js
@@ -4,5 +4,5 @@ import db from './db'
 import { ipcRenderer } from 'electron'
 import * as Imap from 'poodle-service/lib/ImapInterface/Client'
 
-const imapClient = Imap.NewClient(ipcRenderer)
+const imapClient = Imap.NewClient(ipcRenderer, db)
 export default imapClient
