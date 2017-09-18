c8dd872152de4de1d5f010e4877faf3934f52cba
Jesse Hallett <jesse@sitr.us>
2017-09-18 10:15:55 -0400

    poodle-electron: create database indexes

--- packages/poodle-electron/src/db.js
+++ packages/poodle-electron/src/db.js
@@ -3,6 +3,7 @@
 import cachedir from 'cache-directory'
 import mkdirp from 'mkdirp'
 import * as path from 'path'
+import { createIndexes } from 'poodle-service/lib/cache/query'
 import findPlugin from 'pouchdb-find'
 import PouchDB from 'pouchdb-node'
 
@@ -11,4 +12,5 @@ PouchDB.plugin(findPlugin)
 const location = cachedir(path.join('poodle', 'db'))
 mkdirp.sync(location)
 const db = new PouchDB(location, { adapter: 'leveldb' })
+createIndexes(db)
 export default db
