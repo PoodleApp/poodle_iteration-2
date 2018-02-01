3a7c6e1094a955619190c5c9bb9bd03c6351da03
Jesse Hallett <jesse@sitr.us>
2018-02-01 16:17:53 -0400

    poodle-service: UID key range bounds should be closed, not open

--- packages/poodle-service/src/cache/query.js
+++ packages/poodle-service/src/cache/query.js
@@ -104,9 +104,7 @@ export function identifyMissingUids (
   const [min, max] = findExtrema(uids)
   const keyRange = DB.IDBKeyRange.bound(
     [boxName, uidvalidity, min],
-    [boxName, uidvalidity, max],
-    true,
-    true
+    [boxName, uidvalidity, max]
   )
   const stream = DB.txStream(
     db,
