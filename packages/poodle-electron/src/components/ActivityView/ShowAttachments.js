0458d2abd4a201eb44a16407b618c6b9b5fce4d4
Jesse Hallett <jesse@sitr.us>
2018-02-19 10:53:07 -0400

    poodle-electron: display attachments in cards

--- packages/poodle-electron/src/components/ActivityView/ShowAttachments.js
+++ packages/poodle-electron/src/components/ActivityView/ShowAttachments.js
@@ -3,6 +3,8 @@
 import type DerivedActivity from 'arfe/lib/models/DerivedActivity'
 import type Message from 'arfe/lib/models/Message'
 import * as Part from 'arfe/lib/models/MessagePart'
+import { Card, CardActions, CardMedia, CardTitle } from 'material-ui/Card'
+import FlatButton from 'material-ui/FlatButton'
 import * as m from 'mori'
 import { type Account } from 'poodle-core/lib/actions/auth'
 import * as actions from 'poodle-core/lib/view/actions'
@@ -14,40 +16,55 @@ type Props = {
   dispatch: Function
 }
 
-export default function ShowAttachments ({
-  account,
-  activity,
-  dispatch
-}: Props) {
-  const message = activity.message
-  const attachments = activity.attachments
+export default function ShowAttachments (props: Props) {
+  const message = props.activity.message
+  const attachments = props.activity.attachments
   if (!message) {
     return <section />
   }
-
-  function openAttachment (attachment: Part.MessagePart, event: Event) {
-    event.preventDefault()
-    dispatch(actions.openAttachment({ message, attachment, account }))
-  }
-
-  const as = m.map(a => {
-    const uri = message.uriForPart(a)
-    const filename = a.params && a.params.name
-    return (
-      <div key={uri}>
-        <a
-          href={uri}
-          download={filename || 'file'}
-          onClick={event => openAttachment(a, event)}
-        >
-          {filename || 'file'} ({Part.contentType(a)}; {a.size} bytes)
-        </a>
-      </div>
-    )
-  }, attachments)
+  const as = m.map(
+    a => <Attachment attachment={a} message={message} {...props} />,
+    attachments
+  )
   return (
     <section>
       {(m.intoArray(as): any)}
     </section>
+  )
+}
+
+type AttachmentProps = {
+  account: Account,
+  attachment: Part.MessagePart,
+  dispatch: Function,
+  message: Message
+}
+
+function Attachment ({
+  account,
+  attachment,
+  dispatch,
+  message
+}: AttachmentProps) {
+  const filename = (attachment.params && attachment.params.name) || 'file'
+  const mediaType = Part.contentType(attachment)
+  const size = attachment.size ? `${attachment.size} bytes` : ''
+
+  function openAttachment (event: Event) {
+    event.preventDefault()
+    dispatch(actions.openAttachment({ message, attachment, account }))
+  }
+
+  return (
+    <Card>
+      <CardTitle
+        title={filename || 'file'}
+        subtitle={`${mediaType}; ${size}`}
+      />
+      <CardActions>
+        <FlatButton label='View' onClick={openAttachment} />
+        <FlatButton label='Save' disabled={true} />
+      </CardActions>
+    </Card>
   )
 }
